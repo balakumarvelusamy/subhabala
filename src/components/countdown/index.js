85eb84ec6d5bdec50c0ecc4b8617a5e23749d796
bala <vbalakumar.cse@gmail.com>
2023-02-12 23:36:07 -0500

    check in

--- src/components/countdown/index.js
+++ src/components/countdown/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-const {useState, useEffect} = React;
+import React from "react";
+const { useState, useEffect } = React;
 
 const TimeCountDown = () => {
-  const [countdownDate, setCountdownDate] = useState(new Date('06/06/2023').getTime());
+  const [countdownDate, setCountdownDate] = useState(new Date("06/05/2023").getTime()); //mm/dd/yyyy
   const [state, setState] = useState({
     days: 0,
     hours: 0,
@@ -21,12 +21,8 @@ const TimeCountDown = () => {
       const distanceToDate = countdownDate - currentTime;
 
       let days = Math.floor(distanceToDate / (1000 * 60 * 60 * 24));
-      let hours = Math.floor(
-        (distanceToDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-      );
-      let minutes = Math.floor(
-        (distanceToDate % (1000 * 60 * 60)) / (1000 * 60),
-      );
+      let hours = Math.floor((distanceToDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let minutes = Math.floor((distanceToDate % (1000 * 60 * 60)) / (1000 * 60));
       let seconds = Math.floor((distanceToDate % (1000 * 60)) / 1000);
 
       const numbersToAddZeroTo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -46,21 +42,21 @@ const TimeCountDown = () => {
 
   return (
     <div>
-      <div className='countdown-wrapper'>
-        <div className='time-section'>
-          <div className='time'>{state.days || '0'}</div>
+      <div className="countdown-wrapper">
+        <div className="time-section">
+          <div className="time">{state.days || "0"}</div>
           <small className="time-text">Days</small>
         </div>
-        <div className='time-section'>
-          <div className='time'>{state.hours || '00'}</div>
+        <div className="time-section">
+          <div className="time">{state.hours || "00"}</div>
           <small className="time-text">Hours</small>
         </div>
-        <div className='time-section'>
-          <div className='time'>{state.minutes || '00'}</div>
+        <div className="time-section">
+          <div className="time">{state.minutes || "00"}</div>
           <small className="time-text">Min</small>
         </div>
-        <div className='time-section'>
-          <div className='time'>{state.seconds || '00'}</div>
+        <div className="time-section">
+          <div className="time">{state.seconds || "00"}</div>
           <small className="time-text">Sec</small>
         </div>
       </div>
@@ -69,4 +65,3 @@ const TimeCountDown = () => {
 };
 
 export default TimeCountDown;
-
